c00455ab72e13a5310cb5c1b3eab81286a9487a4
Terry Filiba <tfiliba@gmail.com>
2009-08-13 19:58:04 +0000

    Changing nc commands for mac

--- server_software/configure/adc_to_10gbe_dumpbram.sh
+++ server_software/configure/adc_to_10gbe_dumpbram.sh
@@ -4,10 +4,10 @@ IP=$ADC_TO_10GBE_IP
 PORT=7
 OUT_DIR=out
 
-echo "regwrite reg_adcscope_trigger 1" | nc -u -q 1 $IP $PORT
+echo "regwrite reg_adcscope_trigger 1" | nc -u -w 1 $IP $PORT
 sleep 1
-echo "regwrite reg_adcscope_trigger 0" | nc -u -q 1 $IP $PORT
-echo "bramdump bram_adcscope_1" | nc -u -q 7 $IP $PORT > $OUT_DIR/bram_adcscope_1
+echo "regwrite reg_adcscope_trigger 0" | nc -u -w 1 $IP $PORT
+echo "bramdump bram_adcscope_1" | nc -u -w 7 $IP $PORT > $OUT_DIR/bram_adcscope_1
 #./bram2dec.sh $OUT_DIR/bram_adcscope_1
-echo "bramdump bram_adcscope_2" | nc -u -q 7 $IP $PORT > $OUT_DIR/bram_adcscope_2
+echo "bramdump bram_adcscope_2" | nc -u -w 7 $IP $PORT > $OUT_DIR/bram_adcscope_2
 
