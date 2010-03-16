3f7901ea8acebce366fd54dc1edf8dac52cd3b7e
Terry Filiba <tfiliba@gmail.com>
2010-03-16 23:43:47 +0000

    Refactoring code

--- server_software/configure_roach/pasp_init.py
+++ server_software/configure_roach/pasp_init.py
@@ -4,11 +4,14 @@ import corr, time, struct, sys, logging, socket, re
 
 
 # fft scaling configuration
-fft_coeffs=[4096,4096,4096,4096,4096,4096,4096,4096]
+fft_coeffs=[1,1,1,1,1,1,1,1]
 
 # ip table configuration
 ip_table=['10.0.0.14','10.0.0.14','10.0.0.14','10.0.0.14']
 port_table=[6000,6000,6000,6000]
+
+# default boffile
+defaultbof='pasp_4i16c1024s4g_2010_Mar_10_0117.bof'
 
 ip_reg_base='pasp_dist_gbe_ip_ctr_reg_ip'
 port_reg_base='pasp_dist_gbe_ip_ctr_reg_port'
@@ -17,8 +20,8 @@ port_reg_base='pasp_dist_gbe_ip_ctr_reg_port'
 gbe_base='pasp_dist_gbe_ten_GbE'
 
 mac_base=(2<<40) + (2<<32)
-fabric_ip=struct.unpack('=L',socket.inet_aton('10.0.0.30'))[0] #convert ip to long
-fabric_port=60000 
+fabric_ip=struct.unpack('!L',socket.inet_aton('10.0.0.30'))[0] #convert ip to long
+fabric_port=6000
 
 
 katcp_port=7147
@@ -39,6 +42,7 @@ def exit_clean():
     except: pass
     #exit()
     
+# functions for dumping data    
 def dump_adc_brams():
     #trigger the adcscope
     fpga.write_int('pasp_reg_adcscope_trigger',1)
@@ -51,6 +55,7 @@ def dump_adc_brams():
     print adcscope1
     print adcscope2
 
+
 def dump_fft_brams():
     fftscope1 = struct.unpack('>2048b',fpga.read('pasp_scope_output1_bram',2048))
     fftscope2 = struct.unpack('>2048b',fpga.read('pasp_scope_output2_bram',2048))
@@ -61,6 +66,46 @@ def dump_fft_brams():
     print fftscope2
     print fftscope3
     print fftscope4
+   
+def configure_parameters():
+    # initialize the fpga sync
+    logger.debug('Configuring a sync period of %d'%(sync_period))
+    fpga.write_int('pasp_reg_sync_period',sync_period)
+    
+    # initialize the fft shift schedule
+    #fpga.write_int('pasp_reg_fft_shift',0xffffffff)
+    fpga.write_int('pasp_reg_fft_shift',0x0)
+    
+    # initialize the scaling parameters
+    fft_coeffs_string = struct.pack('>8L',*fft_coeffs)
+    fpga.write('pasp_scale_ctr_pol0_even_bram',fft_coeffs_string)
+    fpga.write('pasp_scale_ctr_pol0_odd_bram',fft_coeffs_string)
+
+    # initialize the scaling bit selection
+    fpga.write_int('pasp_reg_output_bitselect_pol1',2)
+    fpga.write_int('pasp_reg_output_bitselect_pol2',2)
+
+      
+def init_10gbe_blocks():
+    # initialize the 10gbe ports
+    for i in range(0,numtengbe):
+        logger.debug('Initializing '+gbe_base+str(i))
+        fpga.tap_start(gbe_base+str(i),mac_base+fabric_ip+i,fabric_ip+i,fabric_port)
+    
+def init_ip_table():
+    # initialize the ip table
+    logger.debug('Initializing ip table')
+    for i in range(0,numips):
+        fpga.write_int(ip_reg_base+str(i),struct.unpack('!L',socket.inet_aton(ip_table[i]))[0])
+        fpga.write_int(port_reg_base+str(i),port_table[i])
+
+        
+def clear_ip_table():
+    # zero the ip table
+    print 'Zeroing ip table'
+    for i in range(0,numips):
+        fpga.write_int(ip_reg_base+str(i),0)
+    
     
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -69,7 +114,7 @@ if __name__ == '__main__':
     p.set_usage('pasp_init.py <ROACH_HOSTNAME_or_IP> [options]')
     p.set_description(__doc__)
     p.set_defaults(interactive_mode=False)
-    p.set_defaults(boffile='pasp_4i16c1024s4g.bof')
+    p.set_defaults(boffile=defaultbof)
     p.add_option('-i','--interactive', help='select boffile interactively', action='store_true', dest='interactive_mode')
     p.add_option('-f','--file', help='use specified boffile', action='store', type='string', dest='boffile')
  
@@ -109,7 +154,7 @@ try:
     
     
     # extract build configuration info from boffile name
-    m=re.search('pasp_([\d]+)i([\d]+)c([\d]+)s([\d]+)g.bof',opts.boffile)
+    m=re.search('pasp_([\d]+)i([\d]+)c([\d]+)s([\d]+)g',opts.boffile)
     
     # get pasp build configuration from boffile name
     numips=int(m.group(1))
@@ -134,52 +179,21 @@ try:
     time.sleep(10)
     print 'Programming complete'
     
+    configure_parameters()
+    
     #print fpga.listdev()
     #print sorted(fpga.listdev())
     #sys.stdout.flush()
     
-    # initialize the fpga sync
-    logger.debug('Configuring a sync period of %d'%(sync_period))
-    fpga.write_int('pasp_reg_sync_period',sync_period)
-    
-    #dump_adc_brams()
-    
-    # initialize the fft shift schedule
-    fpga.write_int('pasp_reg_fft_shift',0xffffffff)
-    
-    # initialize the scaling parameters
-    fft_coeffs_string = struct.pack('>8L',*fft_coeffs)
-    fpga.write('pasp_scale_ctr_pol0_even_bram',fft_coeffs_string)
-    fpga.write('pasp_scale_ctr_pol0_odd_bram',fft_coeffs_string)
-
-    # initialize the scaling bit selection
-    fpga.write_int('pasp_reg_output_bitselect_pol1',1)
-    fpga.write_int('pasp_reg_output_bitselect_pol2',1)
-    
     #dump_fft_brams()
 
-    
-    # initialize the 10gbe ports
-    for i in range(0,numtengbe):
-        logger.debug('Initializing '+gbe_base+str(i))
-        fpga.tap_start(gbe_base+str(i),mac_base+fabric_ip+i,fabric_ip+i,fabric_port)
-        
-            
-    # initialize the ip table
-    logger.debug('Initializing ip table')
-    for i in range(0,numips):
-        fpga.write_int(ip_reg_base+str(i),struct.unpack('=L',socket.inet_aton(ip_table[i]))[0])
-        fpga.write_int(port_reg_base+str(i),port_table[i])
+    init_10gbe_blocks()
+    init_ip_table()
     
     # start recieve code
     raw_input('Press enter to quit: ')
-        
-    # zero the ip table
-    print 'Zeroing ip table'
-    for i in range(0,numips):
-        fpga.write_int(ip_reg_base+str(i),0)
             
-    
+    clear_ip_table()
     
 
 except KeyboardInterrupt:
