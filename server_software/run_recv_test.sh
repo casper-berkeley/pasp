2ee041dfcc249a0a788437692208581b0dc12709
Terry Filiba <tfiliba@gmail.com>
2010-06-09 22:41:58 +0000

    Adding library option to launch script

--- server_software/run_recv_test.sh
+++ server_software/run_recv_test.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-platform=$1
+platform=roach
+software_dir=software
+fftlib=japan
 
-taskset 04 nice -n -20 software/pasp_recv >out/recv_out 2>out/recv_err &
-taskset 08 nice -n -20 software/pasp_process >out/process_out 2>out/process_err &
-#nice -n -20 software/pasp_recv >out/recv_out 2>out/recv_err &
-#nice -n -20 software/pasp_process >out/process_out 2>out/process_err &
+taskset 02 nice -n -20 ${software_dir}/pasp_recv >out/recv_out 2>out/recv_err &
+taskset 04 nice -n -20 ${software_dir}/pasp_distribute >out/distribute_out 2>out/distribute_err &
+taskset 08 nice -n -20 ${software_dir}/${fftlib}_pasp_process >out/process_out 2>out/process_err &
+#nice -n -20 ${software_dir}/pasp_recv >out/recv_out 2>out/recv_err &
+#nice -n -20 ${software_dir}/pasp_process >out/process_out 2>out/process_err &
 
 
 echo configure_${platform}
@@ -14,10 +17,11 @@ cd configure_${platform}
 
 cd ..
 
-echo "sleep 600 to wait for threads to finish"
-sleep 600
+echo "sleep 60 to wait for threads to finish"
+sleep 60
 
-killall -INT software/pasp_process
-killall -INT software/pasp_recv
+killall -INT ${software_dir}/${fftlib}_pasp_process
+killall -INT ${software_dir}/pasp_distribute
+killall -INT ${software_dir}/pasp_recv
 
 echo "regread pasp/dist_gbe/packet_count" | nc -u -w 1 192.168.0.4 7
